16f705716eb04e258e47425fcdc150d7395e60b3
Rafa <rpadiper@gmail.com>
2022-04-21 17:19:12 -0700

    Working on generate maze function for implementing prim's algorithm

--- RafaMaze.py
+++ RafaMaze.py
@@ -12,6 +12,42 @@ class Maze():
 
         maze = [[True for x in range(width)] for y in range(height)]
         
+        def GenerateMaze():
+            # Pick a random starting point
+            startX = int(random.random() * height)
+            startY = int(random.random() * width)
+
+            # Make sure we don't start at the edge of the maze
+            if startY == 0:
+                startY += 1
+            if startY == height-1:
+                startY -= 1
+
+            if startX == 0:
+                startX += 1
+            if startX == width-1:
+                startX -= 1
+
+            # Mark this point as path, add all 4 surrounding walls (+x, +y, -x, -y)
+            maze[startX][startY] = False
+            walls = []
+            walls.append([startY, startX - 1])
+            walls.append([startY, startX + 1])
+            walls.append([startY - 1, startX])
+            walls.append([startY + 1, startX])
+
+            # Make sure surrounding blocks are walls
+            maze[startY, startX - 1] = True
+            maze[startY, startX + 1] = True
+            maze[startY - 1, startX] = True
+            maze[startY + 1, startX] = True
+
+            # While there are still walls, pick a random one
+            while walls:
+                rand_wall = walls[int(random.random()*len(walls))-1]
+                
+            
+
         def RenderMaze():
             for x in range(0, len(maze)):
                 for y in range(0, len(maze[0])):
